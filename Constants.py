214d4f527b1b5ff09014721baff33ff214e0fcb7
shubha-1985 <shubha.lakshmi@optym.com>
2018-03-13 14:13:07 +0530

    Utilities and Classes related to Sector Clustering

---
+++ Constants.py
@@ -0,0 +1,25 @@
+import math
+from enum import Enum
+
+
+class SectorClusteringMethod(Enum):
+    FARTHEST_FROM_DEPOT = 1
+    SMALLEST_ANGLE_fROM_DEPOT = 2
+    EARLIST_DEADLINE = 3
+    LARGEST_SHIPMENT_CAPACITY = 4
+
+
+class PolarCoordiantesConstants(object):
+    CONST_MIN_POLAR_ANGLE_IN_RADIAN = 0.0
+    CONST_MAX_POLAR_ANGLE_IN_RADIAN = 2 * math.pi
+
+
+class Generic(object):
+    CONST_DOUBLE_EPSILON = 0.0001
+
+
+class SectorClusteringConstants():
+    CONST_ANGULAR_SEED_IN_RADIAN = 0.0
+    CONST_ANGULAR_SECTOR_SIZE_IN_RADIAN = 2 * math.pi
+    CONST_ANGULAR_STEP_SIZE_IN_RADIAN = CONST_ANGULAR_SECTOR_SIZE_IN_RADIAN / 4 * 3
+    CONST_SECTOR_CLUSTERING_METHOD = SectorClusteringMethod.FARTHEST_FROM_DEPOT
---
+++ Tests/test_data_loader.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestData_loader(TestCase):
+    def test_data_loader(self):
+        self.fail()
